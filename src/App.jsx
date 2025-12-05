bc8cff8d2e5952bb31d7c9abae570f91be389daf
Abhi290703 <abhigec6@gmail.com>
2025-12-05 21:29:40 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,27 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Blog from "./pages/Blog";
-import Auth from "./components/Auth"; // if Auth stays in components
 import Mainlayout from "./layout/mainlayout";
 import './App.css';
+import { regexConditions } from "./Regex/regexCondition";
+import Auth from "./components/Auth";
+import Login from "./loginform/login";
+import Signup from "./loginform/signup";
+
 
 export default function App() {
   return (
+
+      
     <Routes>
       
-      <Route path="/auth" element={<Auth />} />
+      <Route path="/Auth" element={<Auth/>} />
       <Route element={<Mainlayout />}>
-        <Route index element={<Home />} />           {/* path="/" */}
-        <Route path="products" element={<Product />} />
-        <Route path="blog" element={<Blog />} />
+        <Route index element={<Home />} />          
+        <Route path="/Products" element={<Product />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/Signup" element={<Signup/>} />
       </Route>
     </Routes>
   );
