7be6d9edea9b1ad9739c3f78c1d4cbc0cfbed306
Abhi290703 <abhigec6@gmail.com>
2025-12-07 19:02:32 +0530

    add

--- src/components/Auth.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import Login from "../UserForm/Login";
-import Signup from "../loginform/signup";
-
-
-export default function Auth() {
-  const [entry, setEntry] = useState(false);
-  return <> {entry ? <Signup /> : <Login />}</>;
-}
